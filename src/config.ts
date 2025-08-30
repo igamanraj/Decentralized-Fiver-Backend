2f62f7386ebff35045895d537e68e56274293d69
Aman Raidas <128576848+igamanraj@users.noreply.github.com>
2025-08-30 16:17:33 +0530

    few changes

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const JWT_SECRET = "aman";
 export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
-export const TOTAL_DECIMALS = 1000_000_000;
+export const TOTAL_DECIMALS = 1000_000;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import userRouter from "./routers/user";
 import workerRouter from "./routers/worker";
-
+import cors from "cors";
 
 
 const app = express();
+
+app.use(cors())
+
 
 app.use(express.json());    
 app.use("/v1/user", userRouter);
